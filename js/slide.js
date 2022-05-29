331c423e42280b926b0f735f34471c978a8be58c
lucazdj <lucasdj98@gmail.com>
2022-05-29 00:49:58 -0400

    added config

--- js/slide.js
+++ js/slide.js
@@ -16,24 +16,34 @@ export default class Slide {
   }
 
   onStart(event) {
-    event.preventDefault();
-    this.distance.startX = event.clientX;
-    this.wrapper.addEventListener('mousemove', this.onMove);
+    const movetype = (event.type === 'mousedown') ? 'mousemove' : 'touchmove';
+    console.log(movetype);
+    const pointerPosition = (event.type === 'mousedown') ? event.clientX : event.changedTouches[0].clientX;
+    console.log(pointerPosition);
+    if (event.type === 'mousedown') {
+      event.preventDefault();
+    }
+    this.distance.startX = pointerPosition;
+    this.wrapper.addEventListener(movetype, this.onMove);
   }
 
   onMove(event) {
-    const finalPosition = this.updatePosition(event.clientX);
+    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPosition);
     this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
-    this.wrapper.removeEventListener('mousemove', this.onMove);
+    const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
+    this.wrapper.removeEventListener(movetype, this.onMove);
     this.distance.finalPosition = this.distance.movePosition;
   }
 
   addEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('touchstart', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
+    this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
   bindEvens() {
@@ -42,9 +52,40 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidesNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index === 0 ? undefined : index - 1,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    }
+  }
+
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosition(element);
+      return { position, element, };
+    });
+  }
+
+  changeSlide(index) {
+    console.log(index);
+    const activeSlide = this.slideArray[index];
+    console.log(this.slideArray);
+    this.moveSlide(activeSlide.position);
+    this.slidesNav(index);
+    this.distance.finalPosition = activeSlide.position;
+  }
+
   init() {
     this.bindEvens();
     this.addEvents();
+    this.slidesConfig();
     return this;
   }
 }
